4cb2d4806c91041d8ac9eec86367e461e8d9611b
ryuusora25 <ryuusora25@gmail.com>
2020-12-13 20:46:35 +0700

    cetak persetujuan

--- app/Http/Controllers/PpdbUserPernyataanController.php
+++ app/Http/Controllers/PpdbUserPernyataanController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ppdb_siswas_pernyataansTable;
 use Illuminate\Http\Request;
 use App\Models\ppdb_siswas_rincians;
 use App\Models\ppdb_siswas_ortus;
@@ -13,6 +14,7 @@ use QrCode;
 
 class PpdbUserPernyataanController extends Controller
 {
+<<<<<<< HEAD
     public function index()
     {
         //
@@ -41,11 +43,44 @@ class PpdbUserPernyataanController extends Controller
     }
 
       /**
+=======
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        // ppdb_siswas_rincians::create($request->all());
+//caribarang
+// $products = DB::select("SELECT products.id,products.nama,products.sku,products.harga_modal
+// ,products.harga_jual,product_units.nama as nama_unit,product_categories.nama as nama_category
+// FROM products,product_units,product_categories
+// WHERE products.product_units_id=product_units.id
+//     AND products.product_categories_id=product_categories.id");
+
+        return redirect('ppdbuser_rincian')->with('status','Data berhasil di tambahkan');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+>>>>>>> 5f9452b0c66d57702e99744859824e96dfbbed0a
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+<<<<<<< HEAD
     public function cetak_pdf()
     {
         $email=Auth::user()->email;
@@ -65,5 +100,55 @@ class PpdbUserPernyataanController extends Controller
 
     	$pdf = PDF::loadview('ppdb.user.pernyataanpdf',['pernyataans'=>$pernyataan],compact('qrcode'))->setPaper('legal', 'potrait');
     	return $pdf->download('lembar-pernyataan-pdf');
+=======
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ppdb_siswas_pernyataansTable  $ppdb_siswas_pernyataansTable
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ppdb_siswas_pernyataansTable $ppdb_siswas_pernyataansTable)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ppdb_siswas_pernyataansTable  $ppdb_siswas_pernyataansTable
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ppdb_siswas_pernyataansTable $ppdb_siswas_pernyataansTable)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ppdb_siswas_pernyataansTable  $ppdb_siswas_pernyataansTable
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ppdb_siswas_pernyataansTable $ppdb_siswas_pernyataansTable)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ppdb_siswas_pernyataansTable  $ppdb_siswas_pernyataansTable
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ppdb_siswas_pernyataansTable $ppdb_siswas_pernyataansTable)
+    {
+        //
+>>>>>>> 5f9452b0c66d57702e99744859824e96dfbbed0a
     }
 }
---
+++ app/Models/ppdb_siswas_pernyataansTable.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ppdb_siswas_pernyataansTable extends Model
+{
+    use HasFactory;
+}
